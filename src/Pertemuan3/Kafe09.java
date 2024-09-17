ebb3f852e04c5c049449e4732badf8f7f026a2ff
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-09-17 19:57:23 +0700

    “P3_Percobaan2”

--- src/Pertemuan3/Kafe09.java
+++ src/Pertemuan3/Kafe09.java
@@ -1,7 +1,30 @@
 package Pertemuan3;
 
+import java.util.Scanner;
+
 public class Kafe09 {
   public static void main(String[] args) {
-    
+    Scanner input = new Scanner(System.in);
+
+    boolean keanggotaan;
+    int jumlahKopi, jumlahTeh, jumlahRoti;
+    double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 2000.0, totalHarga, nominalBayar;
+    float diskon = 10 / 100f;
+
+    System.out.print("Masukan keanggotaan (true/false): ");
+    keanggotaan = input.nextBoolean();
+
+    System.out.print("Masukan jumlah pembelian kopi: ");
+    jumlahKopi = input.nextInt();
+    System.out.print("Masukan jumlah pembelian teh: ");
+    jumlahTeh = input.nextInt();
+    System.out.print("Masukan jumlah pembelian rpto: ");
+    jumlahRoti = input.nextInt();
+
+    totalHarga = (jumlahKopi * hargaKopi) + (jumlahTeh * hargaTeh) + (jumlahRoti * hargaRoti);
+    nominalBayar = totalHarga - (diskon*totalHarga);
+    System.out.println("Keanggotaan pelanggan " + keanggotaan);
+    System.out.println("Item pemblian " + jumlahKopi + " kopi, "+ jumlahTeh +  " teh, " + jumlahRoti + " roti");
+    System.out.println("Nominal bayar Rp " + nominalBayar);
   }
 }
