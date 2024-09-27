9dde0f92a45b78c275f5770914aaf49eeb9efdbd
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-09-27 17:31:45 +0700

    “P5_Percobaan3_PertanyaanNo4”

---
+++ src/Pertemuan5/PemilihanHariDenganIf09.java
@@ -0,0 +1,23 @@
+package Pertemuan5;
+
+import java.util.Scanner;
+
+public class PemilihanHariDenganIf09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        byte hari;
+        String dayType = null;
+
+        System.out.print("Masukan hari : ");
+        hari = input.nextByte();
+
+        if (hari == 1 || hari == 2 || hari == 3 || hari == 4 || hari == 5) {
+            dayType = "Weekday";
+        } else if (hari == 6 || hari == 7) {
+            dayType = "Weekend";
+        }
+
+        System.out.println(dayName + " is a " + dayType);
+    }
+
+}
