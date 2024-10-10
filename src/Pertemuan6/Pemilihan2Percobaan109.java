db5a6ad2765811776041e5cf0d3e3d82db92d13b
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-10-10 16:42:59 +0700

    percobaan 1 pertanyaan no 1

---
+++ src/Pertemuan6/Pemilihan2Percobaan109.java
@@ -0,0 +1,21 @@
+package Pertemuan6;
+
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan109 {
+  public static void main(String[] args) {
+    Scanner input09 = new Scanner(System.in);
+    int tahun;
+    System.out.print("Masukan tahun : ");
+    tahun = input09.nextInt();
+    if ((tahun % 4) == 0) {
+      if ((tahun % 100) != 0 || (tahun % 400) == 0) {
+        System.out.println("Tahun kabisat");
+      } else {
+        System.out.println("Bukan tahun kabisat");
+      }
+    } else {
+      System.out.println("Bukan tahun kabisat");
+    }
+  }
+}
