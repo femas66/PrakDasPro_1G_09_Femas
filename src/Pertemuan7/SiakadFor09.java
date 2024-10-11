efd1a72f783cb83a7322234bf59ed73e60084e24
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-10-11 13:47:33 +0700

    percobaan 1 pertanyaan no 5

--- src/Pertemuan7/SiakadFor09.java
+++ src/Pertemuan7/SiakadFor09.java
@@ -10,11 +10,16 @@ public class SiakadFor09 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     double nilai, tertinggi = 0, terendah = 100;
+    byte lolos = 0, gagal = 0;
 
     for (int i = 1; i <= 10; i++) {
       System.out.print("Masukan nilai mahasiswa ke - " + i + " : ");
       nilai = sc.nextDouble();
-
+      if (nilai >= 60) {
+        lolos++;
+      } else {
+        gagal++;
+      }
       if (nilai > tertinggi) {
         tertinggi = nilai;
       }
@@ -25,5 +30,7 @@ public class SiakadFor09 {
 
     System.out.println("Nilai tertinggi : " + tertinggi);
     System.out.println("Nilai terendah : " + terendah);
+    System.out.println("Gagal : " + gagal);
+    System.out.println("Lolos : " + lolos);
   }
 }
