879f7fa8ba97dfc48d668c2872a73386b609b0d7
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-11-01 15:21:54 +0700

    P10 Percobaan 3 Pertanyaan nomor 3

--- src/Pertemuan10/ArrayRataNilai09.java
+++ src/Pertemuan10/ArrayRataNilai09.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class ArrayRataNilai09 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int[] nilaiMhs = new int[10];
+    int panjang;
+    System.out.print("Masukan jumlah mahasiswa : ");
+    panjang = sc.nextInt();
+    int[] nilaiMhs = new int[panjang];
     double total = 0;
     double rata2;
+    int banyakLolos = 0;
 
     for (int i = 0; i < nilaiMhs.length; i++) {
       System.out.print("Masukan nilai mahasiswa ke " + (i + 1) + " : ");
@@ -16,9 +20,11 @@ public class ArrayRataNilai09 {
 
     for (int i = 0; i< nilaiMhs.length; i++) {
       total += nilaiMhs[i];
+      if (nilaiMhs[i] > 70) banyakLolos++;
     }
 
     rata2 = total/nilaiMhs.length;
+    System.out.println("Jumlah lolos : " + banyakLolos);
     System.out.println("Rata rata nilai adalah : " + rata2);
   }
 }
