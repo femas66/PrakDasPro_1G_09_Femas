8be5a03eefba19d8260666d230143c6359a1aad1
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-11-01 14:55:32 +0700

    Percobaan 2

---
+++ src/Pertemuan10/ArrayNilai09.java
@@ -0,0 +1,18 @@
+package Pertemuan10;
+
+import java.util.Scanner;
+
+public class ArrayNilai09 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int[] nilaiAkhir = new int[10];
+
+    for (int i = 0; i < 10; i++) {
+      System.out.print("Masukan nilai akhir ke " + i + " : ");
+      nilaiAkhir[i] = sc.nextInt();
+    }
+    for (int i = 0; i< 10; i++) {
+      System.out.println("Nilai akhir ke " + i + " : " + nilaiAkhir[i]);
+    }
+  }
+}
