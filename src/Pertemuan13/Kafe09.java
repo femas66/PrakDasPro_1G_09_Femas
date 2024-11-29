b3886fc9a040dde70e0e6e5bdf69d65b4b05ab45
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-11-29 14:00:41 +0700

    Percobaan 3

--- src/Pertemuan13/Kafe09.java
+++ src/Pertemuan13/Kafe09.java
@@ -1,6 +1,14 @@
 package Pertemuan13;
 
+import java.util.Scanner;
+
 public class Kafe09 {
+
+  public static int hitungTotalHarga09(int pilihanMenu, int banyakItem) {
+    int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+    int hargaTotal = hargaItems[pilihanMenu-1] * banyakItem;
+    return hargaTotal;
+  }
 
   public static void Menu(String namaPelanggan, boolean isMember,String kodepromo) {
     System.out.println("Selamat datang, " + namaPelanggan + "!");
@@ -27,6 +35,15 @@ public class Kafe09 {
   }
   
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
    Menu("Andi", true, "DISKON50"); 
+    System.out.print("\nMasukan nomor menu yang ingin anda pesan : ");
+    int pilihanMenu = sc.nextInt();
+
+    System.out.print("Masukan jumlah item yang ingin dipesan: ");
+    int banyakItem = sc.nextInt();
+
+    int totalHarga = hitungTotalHarga09(pilihanMenu, banyakItem);
+    System.out.println("Total harga untuk pesanan anda : Rp " + totalHarga);
   }
 }
